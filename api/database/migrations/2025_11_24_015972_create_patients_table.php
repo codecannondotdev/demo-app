2b599cdf4a39d208ffc73d2c25dcce5d2a9f27d3
Codecannon <generator@codecannon.dev>
2025-11-24 21:59:05 +0000

    feat: Generate modules

---
+++ api/database/migrations/2025_11_24_015972_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\PatientGenderEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->enum('gender', array_map(fn ($case) => $case->value, PatientGenderEnum::cases()));
+            $table->string('contact_number');
+            $table->string('email_address');
+            $table->longText('address')->nullable();
+            $table->string('emergency_contact_name')->nullable();
+            $table->string('emergency_contact_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
---
+++ ui/src/views/Treatment/List.vue
@@ -0,0 +1,65 @@
+<template>
+	<Header title="Treatments">
+		<Button
+			icon="fal fa-plus"
+			label="Create"
+			@click="router.push({ name: 'treatments-create' })" />
+	</Header>
+	<Container>
+		<ListSearch
+			:list-state="listState"
+			placeholder="Search Treatments" />
+	</Container>
+	<Container>
+		<ApiTable :list-state="listState">
+			<Column header="Treatment Date">
+				<template #body="slotProps">
+					{{
+						slotProps.data.treatment_date
+							? format(new Date(slotProps.data.treatment_date), 'yyyy-MM-dd')
+							: ''
+					}}
+				</template>
+			</Column>
+			<Column
+				field="description"
+				header="Description" />
+			<Column
+				field="outcome"
+				header="Outcome" />
+			<Column
+				field="cost"
+				header="Cost" />
+			<Column
+				:style="{ maxWidth: '92px', width: '92px' }"
+				header="">
+				<template #body="columnProps">
+					<ApiTableLinkButton
+						:to="{ name: 'treatments-edit', params: { id: columnProps.data.id } }"
+						icon="fal fa-arrow-up-right-from-square" />
+					<ApiTableRemoveButton :item="columnProps.data" />
+				</template>
+			</Column>
+		</ApiTable>
+	</Container>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue'
+import { onBeforeMount } from 'vue'
+import { useRouter } from 'vue-router'
+import useApiTable from '@/components/Table/useApiTable'
+import Container from '@/components/Container.vue'
+import ListSearch from '@/components/ListSearch.vue'
+import Button from 'primevue/button'
+import { useTreatmentListState } from '@/models/Treatment/States'
+import { format } from 'date-fns'
+
+const router = useRouter()
+const listState = useTreatmentListState()
+const { ApiTable, Column, ApiTableLinkButton, ApiTableRemoveButton } = useApiTable(listState)
+
+onBeforeMount(() => {
+	listState.getList()
+})
+</script>
